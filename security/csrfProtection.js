4d617f4390dc0d0f76b48ebe42f49ee35484a7f6
Minez454 <141624041+Minez454@users.noreply.github.com>
2026-03-31 14:51:30 -0700

    Add CSRF protection middleware

---
+++ security/csrfProtection.js
@@ -0,0 +1,8 @@
+// csrfProtection.js
+// Security features to protect against CSRF attacks
+
+function csrfProtection(req, res, next) {
+    // Implementation of CSRF protection...
+}
+
+module.exports = csrfProtection;
