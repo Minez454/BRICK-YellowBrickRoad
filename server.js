1e8c034b293882f9c1310d219eade8baef1f77ec
Minez454 <141624041+Minez454@users.noreply.github.com>
2026-04-04 06:47:20 -0700

    Add CSRF protection and cookie parser middleware

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 require('dotenv').config();
-
+const csrfProtection = require('./security/csrfProtection');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -13,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors());
 app.use(morgan('combined'));
-
+app.use(cookieParser());
+app.use(csrfProtection);
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
